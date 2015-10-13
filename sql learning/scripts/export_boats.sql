ca4b3cb60c064f20e80850ead1605d9666cd0aa5
harirajan.ambattumyalil <harirajan.ambattumyalil@zafin.com>
2015-10-13 14:09:57 +0530

    sql files checkin

---
+++ sql learning/scripts/export_boats.sql
@@ -0,0 +1,9 @@
+--------------------------------------------------------
+--  File created - Thursday-June-18-2015   
+--------------------------------------------------------
+REM INSERTING into BOATS
+SET DEFINE OFF;
+Insert into BOATS (BID,BNAME,COLOR) values (101,'Interlake','blue');
+Insert into BOATS (BID,BNAME,COLOR) values (102,'Interlake','red');
+Insert into BOATS (BID,BNAME,COLOR) values (103,'Clipper','green');
+Insert into BOATS (BID,BNAME,COLOR) values (104,'Marine','red');
---
+++ sql learning/scripts/export_sailors.sql
@@ -0,0 +1,14 @@
+--------------------------------------------------------
+--  File created - Thursday-June-18-2015   
+--------------------------------------------------------
+REM INSERTING into SAILORS
+SET DEFINE OFF;
+Insert into SAILORS (SID,SNAME,RATING,AGE) values (22,'Dustin',7,45);
+Insert into SAILORS (SID,SNAME,RATING,AGE) values (29,'Brutus',1,33);
+Insert into SAILORS (SID,SNAME,RATING,AGE) values (31,'Lubber',8,56);
+Insert into SAILORS (SID,SNAME,RATING,AGE) values (32,'Andy',8,26);
+Insert into SAILORS (SID,SNAME,RATING,AGE) values (58,'Rusty',10,35);
+Insert into SAILORS (SID,SNAME,RATING,AGE) values (64,'Horatio',7,35);
+Insert into SAILORS (SID,SNAME,RATING,AGE) values (71,'Zorba',10,16);
+Insert into SAILORS (SID,SNAME,RATING,AGE) values (74,'Horatio',9,35);
+Insert into SAILORS (SID,SNAME,RATING,AGE) values (85,'Art',3,26);
