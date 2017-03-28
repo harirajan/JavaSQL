4336a0317382c5ab4eee0efbe537104f9672462c
hari rajan a <harimac@haris-MacBook-Pro.local>
2017-03-28 07:01:07 +0530

    JDBC connection to VB

--- core_java/src/com/myJava/JDBCConnectionDemo.java
+++ core_java/src/com/myJava/JDBCConnectionDemo.java
@@ -1,10 +1,19 @@
 package com.myJava;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBCConnectionDemo {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		
-		
+		Class.forName("oracle.jdbc.driver.OracleDriver");  
+		  
+		//step2 create  the connection object  
+		Connection con=DriverManager.getConnection(  
+		"jdbc:oracle:thin:@127.0.0.1:1521/orcl","miTest","miTest");  
+		System.out.println("printing"+con);
 	}
 
 }
